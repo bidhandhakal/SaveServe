e9053c257f3905b9fdf083d49e474b88081d6f89
BIDHAN DHAKAL <bidhandhakal.web@gmail.com>
2025-12-29 08:04:32 +0545

    feat: add next/auth

--- src/components/features/landing/HeroSection.tsx
+++ src/components/features/landing/HeroSection.tsx
@@ -1,4 +1,3 @@
-
 import { Button } from "@/components/ui/button";
 
 export default function HeroSection() {
@@ -6,11 +5,14 @@ export default function HeroSection() {
     <div className="w-full">
       <div className="container mx-auto">
         <div className="relative flex flex-col items-center justify-center gap-8 py-0 lg:py-10">
-         <div>
+          <div>
             <h1 className="text-4xl font-bold">SaveServe</h1>
-            <p className="text-lg text-muted-foreground">SaveServe is a platform that helps you save money on your daily expenses.</p>
-         </div>
-         </div>
+            <p className="text-lg text-muted-foreground">
+              SaveServe is a platform that helps you save money on your daily
+              expenses.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
